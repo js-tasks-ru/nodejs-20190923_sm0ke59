c5a6b16bb24c711b11a1bc424d0e79feb6eadb1f
yurlovr <yurlovr@gmail.com>
2019-09-22 12:49:53 +0500

    add solution for 1-module 1-task

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,5 +1,11 @@
 function sum(a, b) {
-  /* ваш код */
+   return isNumber(a) && isNumber(b)
+  ? sumAB(a, b)
+  : throwError()
 }
+
+const isNumber = (n) => typeof n === 'number' && !isNaN(n) && isFinite(n)
+const sumAB = (a, b) => a + b
+const throwError = () => { throw new TypeError() }
 
 module.exports = sum;
