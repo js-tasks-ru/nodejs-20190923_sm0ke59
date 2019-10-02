0182c4475c02b8cdcc68236593b4fca5523ee975
yurlovr <yurlovr@gmail.com>
2019-10-03 00:46:06 +0500

    git commit -am "add solution for 2-module 2-task"

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -3,13 +3,30 @@ const os = require('os');
 
 class LineSplitStream extends stream.Transform {
   constructor(options) {
+    if (options.encoding === 'utf-8' ||
+        options.encoding === 'utf8') {
+      options = {...options, ...{decodeStrings: false}};
+    }
     super(options);
+    this.encoding = options.encoding.replace('-', '');
+    this.buffer = '';
   }
 
   _transform(chunk, encoding, callback) {
+    if (encoding !== this.encoding) throw new TypeError('not UTF-8');
+    this.buffer +=chunk; 
+    if (this.buffer.includes(os.EOL)) {
+      const arrayFromBuffer = this.buffer.split(os.EOL);
+      arrayFromBuffer.filter((item, index) => index < arrayFromBuffer.length - 1).forEach(i => this.push(i))
+      this.buffer = arrayFromBuffer.filter((item, index) => index === arrayFromBuffer.length - 1).join('');
+    }
+    callback();
   }
 
   _flush(callback) {
+    if (this.buffer) {
+      callback(null, this.buffer);
+    }
   }
 }
 
