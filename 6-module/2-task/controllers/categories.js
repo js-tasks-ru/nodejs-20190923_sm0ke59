bb25c6b26e1fbf308f69c5c6553df73fa9c4ac52
yurlovr <yurlovr@gmail.com>
2019-10-14 00:45:38 +0500

    add solution for 6-module 2-task

--- 6-module/2-task/controllers/categories.js
+++ 6-module/2-task/controllers/categories.js
@@ -2,6 +2,5 @@ const Category = require('../models/Category');
 
 module.exports.categoryList = async function categoryList(ctx, next) {
   const categories = await Category.find();
-  console.log({categories})
   ctx.body = {categories} ;
 };
