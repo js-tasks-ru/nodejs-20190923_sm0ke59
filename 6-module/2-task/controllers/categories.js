47b92af164b15885e70378ac9b4ec85b9deb91b5
yurlovr <yurlovr@gmail.com>
2019-10-14 22:14:32 +0500

    add solution for 6-module 2-task

--- 6-module/2-task/controllers/categories.js
+++ 6-module/2-task/controllers/categories.js
@@ -1,6 +1,27 @@
 const Category = require('../models/Category');
 
 module.exports.categoryList = async function categoryList(ctx, next) {
-  const categories = await Category.find();
+  let categories = await Category.find();
+  categories = categories
+    .map(item => {
+      return {...item.toJSON(), id: item._id }
+    })
+    .map( item => { 
+      delete(item._id)
+      delete(item.__v)
+      return item })
+      .map(item => {
+        if(item.subcategories.length) {
+        item.subcategories = item.subcategories.map(i => {
+            return { ...i, id: i._id}
+          })
+          .map(i => {
+            delete(i._id)
+            return i
+          })
+        }
+        return item
+      })
+  console.log(categories);
   ctx.body = {categories} ;
 };
