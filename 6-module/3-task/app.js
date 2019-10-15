863c5a58c97b3cd016f0b74042f2fcccddee3318
yurlovr <yurlovr@gmail.com>
2019-10-15 23:56:07 +0500

    6-module 3-task

--- 6-module/3-task/app.js
+++ 6-module/3-task/app.js
@@ -1,6 +1,8 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const {productsByQuery} = require('./controllers/products');
+// const Category = require('./models/Category');
+// const Product = require('./models/Product');
 
 const app = new Koa();
 
@@ -22,6 +24,35 @@ app.use(async (ctx, next) => {
 const router = new Router({prefix: '/api'});
 
 router.get('/products', productsByQuery);
+// router.post('/createCat', async (ctx) => {
+//   const category = await Category.create({
+//     title: 'Category1',
+//     subcategories: [{
+//       title: 'Subcategory1',
+//     }],
+//   });
+//   ctx.body = category;
+// });
+// router.post('/createProd', async (ctx) => {
+//   const category = await Category.find();
+//   console.log(category[0].subcategories);
+//   const product1 = await Product.create({
+//    title: 'ProductA',
+//       description: 'better than ProductB',
+//       price: 10,
+//       category: category[0].id,
+//       subcategory: category[0].subcategories[0].id,
+//       images: ['image1'],
+//   });
+//    const product2 = await Product.create({
+//    title: 'ProductB',
+//       description: 'better than ProductA',
+//       price: 10,
+//       category: category[0].id,
+//       subcategory: category[0].subcategories[0].id,
+//       images: ['image1'],
+//   });
+// });
 
 app.use(router.routes());
 
