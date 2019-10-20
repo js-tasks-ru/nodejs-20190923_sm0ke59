f36d15c833595fd31f2dc0ee412008c8183a63c1
yurlovr <yurlovr@gmail.com>
2019-10-20 18:18:44 +0500

    7-module 2-task

--- 7-module/2-task/libs/strategies/authenticate.js
+++ 7-module/2-task/libs/strategies/authenticate.js
@@ -1,7 +1,9 @@
 const User = require('../../models/User');
 
 module.exports = async function authenticate(strategy, email, displayName, done) {
-    if (!email) return done(null, false, 'Не указан еmail');
+    if (!email) {
+      done(null, false, 'Не указан email');
+    }
     let user = await User.findOne({email});
     if (!user) {
       try {
