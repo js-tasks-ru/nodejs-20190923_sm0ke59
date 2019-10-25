e2310b5bb5004160e7fdac1ca9685c5aec56c4c5
Roman <r.yurlov@promo-bot.ru>
2019-10-25 14:15:45 +0500

    8-module 1-task

--- 8-module/1-task/controllers/registration.js
+++ 8-module/1-task/controllers/registration.js
@@ -5,7 +5,7 @@ const sendMail = require('../libs/sendMail');
 module.exports.register = async (ctx, next) => {
   const {email, password, displayName} = ctx.request.body;
   const user = await User.findOne({email});
-  if (user) return ctx.throw(400, {errors: {email: 'Такой email уже существует'}});
+  if (user) return ctx.throw(400, {message: {email: 'Такой email уже существует'}});
   const verificationToken = uuid();
   const newUser = new User({
     email,
@@ -28,13 +28,15 @@ module.exports.confirm = async (ctx, next) => {
   const {verificationToken} = ctx.request.body;
   if (!verificationToken) {
     return ctx.throw(400,
-        {errors:
+        {message:
           {error: 'Ссылка подтверждения недействительна или устарела'},
         });
   }
   const user = await User.findOne({verificationToken});
   if (!user) {
-    return ctx.throw(400, {error: 'Ссылка подтверждения недействительна или устарела'});
+    return ctx.throw(400, {message:
+      {error: 'Ссылка подтверждения недействительна или устарела'},
+    });
   }
   user.verificationToken = undefined;
   await user.save();
