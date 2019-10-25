b3ab212ac39eceb1ca9ed3f02b4a94f8299bdd2b
Roman <r.yurlov@promo-bot.ru>
2019-10-25 17:23:29 +0500

    8-module 2-task

--- 8-module/2-task/controllers/orders.js
+++ 8-module/2-task/controllers/orders.js
@@ -3,24 +3,25 @@ const sendMail = require('../libs/sendMail');
 const Product = require('../models/Product');
 
 module.exports.checkout = async function checkout(ctx, next) {
-  const {product, phone, address} = ctx.request.body;
-  const {_id, email} = ctx.user;
-  const order = await Order.create({user: _id, product, phone, address});
-  const productInfo = await Product.findById(product);
+  const {product, phone, address} = ctx.request.body; // получили параметры из запроса
+  const {_id, email} = ctx.user; // получили пользователя
+  const order = await Order.create({user: _id, product, phone, address}); // создаем заказ в базе
+  const productInfo = await Product.findById(product); // ищем продукты в базе по id
 
-  await sendMail({
+  await sendMail({ // отправляем на почту ид заказа и заголовок продукта
     template: 'order-confirmation',
     locals: {id: order._id, product: productInfo},
     to: email,
     subject: 'Подтвердите заказ',
   });
   ctx.status = 200;
-  ctx.body = {order: order._id};
+  ctx.body = {order: order._id}; // отправляем в ответ на запрос ид заказа
 };
 
 module.exports.getOrdersList = async function ordersList(ctx, next) {
+  // ищем заказы и разворачиваем их по product
   const order = await Order.find({user: ctx.user._id}).populate('Product');
-  if (!order) return ctx.throw(400, 'У Вас нет заказов');
+  if (!order) return ctx.throw(400, 'У Вас нет заказов'); // ничего нет
   ctx.status = 200;
-  ctx.body = {orders: order};
+  ctx.body = {orders: order}; // отправляем заказы в виде массива
 };
