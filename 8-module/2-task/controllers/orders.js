4dd6010be75369179860838a01f699f3f60c6d97
Roman <r.yurlov@promo-bot.ru>
2019-10-25 16:39:53 +0500

    8-module 2-task

--- 8-module/2-task/controllers/orders.js
+++ 8-module/2-task/controllers/orders.js
@@ -1,8 +1,25 @@
 const Order = require('../models/Order');
 const sendMail = require('../libs/sendMail');
+const Product = require('../models/Product');
 
 module.exports.checkout = async function checkout(ctx, next) {
+  const {product, phone, address} = ctx.request.body;
+  const user = ctx.user;
+  const order = new Order({user: user._id, product, phone, address});
+  await order.save();
+  console.log(order);
+  const {id} = order;
+  const productInfo = await Product.findById(product);
+
+  await sendMail({
+    template: 'order-confirmation',
+    locals: {id, product: productInfo},
+    to: user.email,
+    subject: 'Подтвердите заказ',
+  });
 };
 
 module.exports.getOrdersList = async function ordersList(ctx, next) {
+  const odrder = await Order.find({user: ctx.user._id}).populate('Product');
+  ctx.body = {odrders: [odrder]};
 };
