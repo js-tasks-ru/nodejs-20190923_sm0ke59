6b6d81d554768101b780d53f50656b0717378870
yurlovr <yurlovr@gmail.com>
2019-10-26 18:17:27 +0500

    9-module 1-task

--- 9-module/1-task/controllers/messages.js
+++ 9-module/1-task/controllers/messages.js
@@ -2,6 +2,7 @@ const Message = require('../models/Message');
 const mapMessage = require('../mappers/message');
 
 module.exports.messageList = async function messages(ctx, next) {
-  const messages = await Message.find();
-  ctx.body = {messages:mapMessage(messages.slice(-20))};
+  const messages = await Message.find({chat: ctx.user.id}).sort({date: 1}).limit(20);
+
+  ctx.body = {messages:mapMessage(messages)};
 };
