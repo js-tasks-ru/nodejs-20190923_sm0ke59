3867845364bab4e3ea257ef6ea0d252f96242ef4
yurlovr <yurlovr@gmail.com>
2019-10-26 17:42:01 +0500

    9-module 1-task

--- 9-module/1-task/controllers/messages.js
+++ 9-module/1-task/controllers/messages.js
@@ -1,5 +1,6 @@
 const Message = require('../models/Message');
 
 module.exports.messageList = async function messages(ctx, next) {
-  ctx.body = {messages: []};
+  const messages = await Message.find();
+  ctx.body = messages.slice(-20);
 };
